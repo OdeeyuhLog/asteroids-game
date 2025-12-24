3249731bbb359170e53256397f2714e13a79b727
Reapur <charlesfreud405@protonmail.com>
2025-12-25 04:06:40 +0800

    Add player-asteroid collision event trigger

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 
 
@@ -33,7 +34,13 @@ def main():
                 return
 
         updatable.update(dt)
-
+        
+        for a in asteroids:
+            if a.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+        
         screen.fill("black")
 
         for d in drawable:
